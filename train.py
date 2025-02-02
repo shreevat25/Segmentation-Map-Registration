9f519385e4f85a5fe423512a6756c3a514c45f3f
karthiknm <83635421+karthiknm@users.noreply.github.com>
2025-02-02 23:03:09 +0530

    Update train.py

--- train.py
+++ train.py
@@ -8,34 +8,38 @@ from tqdm import tqdm
 import numpy as np
 import argparse
 import os
-
-def train(model, stn, dataloader, optimizer, template, device, dice_weight=1.0, smooth_weight=0.1):
+torch.cuda.empty_cache()
+# train.py
+def train(model, stn, dataloader, optimizer, device, dice_weight=1.0, smooth_weight=0.1):
     model.train()
     total_loss = 0
     dice_loss_total = 0
     smooth_loss_total = 0
 
-    for fixed_seg_map, fixed_seg_path in tqdm(dataloader, desc='Training Batches', leave=False):
-        fixed_seg_map = fixed_seg_map.to(device)
-        template = template.to(device)
+    for moving, fixed in tqdm(dataloader, desc='Training Batches', leave=False):  # Fix: Correct variable names
+        moving = moving.to(device)
+        fixed = fixed.to(device)
 
+        # Concatenate moving and fixed along channels
+        input_to_model = torch.cat([moving, fixed], dim=1)  # Shape: (B, 8, 256, 256, 256)
+        
         # Forward pass through U-Net
-        deformation_field = model(template)
+        deformation_field = model(input_to_model)
         
-        # get warped template
-        warped_template = stn(template, deformation_field)
+        # Warp the moving image (template)
+        warped_template = stn(moving, deformation_field)
 
-        dice_loss_val = dice_loss(warped_template, fixed_seg_map)
+        # Compute losses
+        dice_loss_val = dice_loss(warped_template, fixed)
         smooth_loss_val = smoothing_loss(deformation_field)
-        
-        # Composite loss function - for now the weights of the losses are hyperparameters, future they can also be
-        #learnt
         loss = dice_weight * dice_loss_val + smooth_weight * smooth_loss_val
 
+        # Backpropagation
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
+        # Accumulate losses
         total_loss += loss.item()
         dice_loss_total += dice_loss_val.item()
         smooth_loss_total += smooth_loss_val.item()
@@ -44,26 +48,27 @@ def train(model, stn, dataloader, optimizer, template, device, dice_weight=1.0, 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--train_txt', type=str, required=True, help='Path to the training file listing subject paths', default = '/local/scratch/v_karthik_mohan/train_npy.txt')
-    parser.add_argument('--template_path', type=str, required=True, help='Path to the template segmentation map', default = '/local/scratch/v_karthik_mohan/data/OASIS_OAS1_0406_MR1/seg4_onehot.npy')
+    parser.add_argument('--train_txt', type=str, default = '/local/scratch/v_karthik_mohan/train_npy.txt', help='Path to the training file listing subject paths')
+    parser.add_argument('--template_path', type=str, default = '/local/scratch/v_karthik_mohan/data/OASIS_OAS1_0406_MR1/seg4_onehot.npy', help='Path to the template segmentation map')
     parser.add_argument('--lr', type=float, default=1e-4, help='Learning rate')
     parser.add_argument('--batch_size', type=int, default=1, help='Batch size for training')
     parser.add_argument('--epochs', type=int, default=50, help='Number of training epochs')
     parser.add_argument('--save_model_path', type=str, default='./trained_model.pth', help='Path to save the trained model')
     args = parser.parse_args()
 
-    device = 'cuda'
+    device = 'cuda:3'
 
     # Load dataset 
-    train_dataset = SegDataset(args.train_txt)
+    print("Loading dataset")
+    train_dataset = SegDataset(args.train_txt, args.template_path)
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
-
+    print("Loading template. ")
     # Load template: for now using a median file from the dataset itself as a template, can be adjusted.
     template = torch.from_numpy(np.load(args.template_path)).float().unsqueeze(0).to(device)  # Shape (1, 4, 256, 256, 256)
 
     
-    model = UNet(in_channels=4, out_channels=3).to(device)
-    stn = SpatialTransformer(size=(256, 256, 256)).to(device)
+    model = UNet(in_channels=8, out_channels=3).to(device)
+    stn = SpatialTransformer().to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 
     for epoch in tqdm(range(args.epochs), desc='Training Epochs'):
@@ -75,3 +80,6 @@ def main():
 
     torch.save(model.state_dict(), args.save_model_path)
     print(f"Model saved to {args.save_model_path}")
+
+if __name__ == "__main__":
+    main()

c61701f77bb7085a93167ad14e34bad9f186e2b4
karthiknm <83635421+karthiknm@users.noreply.github.com>
2025-02-02 23:40:44 +0530

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -1,12 +1,13 @@
+# get_data.py
 import torch
+import torch.nn.functional as F
 from torch.utils.data import Dataset
 import numpy as np
 import os
-
+#for now resize one hot encoded segmentation maps down to 64^3 otherwise will face memory issues
 class SegDataset(Dataset):
-    def __init__(self, data_list_file: str, template_path: str):
-   
-        # Read subject paths from txt file where each line is a subejct path
+    def __init__(self, data_list_file: str, template_path: str, target_size=(64, 64, 64)):
+        # Read subject paths from txt file where each line is a subject path
         with open(data_list_file, 'r') as file:
             self.subject_paths = file.read().splitlines()
 
@@ -14,17 +15,30 @@ class SegDataset(Dataset):
         self.moving_template = np.load(template_path)
         self.moving_template = torch.tensor(self.moving_template, dtype=torch.float32)
 
+  
+        self.moving_template = F.interpolate(
+            self.moving_template.unsqueeze(0),  
+            size=target_size,
+            mode='trilinear',  
+            align_corners=False
+        ).squeeze(0)  
+
+        self.target_size = target_size
+
     def __len__(self):
         return len(self.subject_paths)
 
     def __getitem__(self, idx):
-    
         fixed_path = self.subject_paths[idx]
         fixed_map = np.load(fixed_path)
 
-        # Convert to tensors
+        
         fixed_map = torch.tensor(fixed_map, dtype=torch.float32)
+        fixed_map = F.interpolate(
+            fixed_map.unsqueeze(0),
+            size=self.target_size,
+            mode='trilinear',  
+            align_corners=False
+        ).squeeze(0)  
 
         return self.moving_template, fixed_map
-
-

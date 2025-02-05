a1046555b4668f13467d66f56e157657ddca1004
karthiknm <83635421+karthiknm@users.noreply.github.com>
2025-02-05 20:46:24 +0530

    Update convert_one_hot.py

--- convert_one_hot.py
+++ convert_one_hot.py
@@ -3,25 +3,22 @@ import numpy as np
 import nibabel as nib
 from tqdm import tqdm
 
-# Paths
+# Txt file where each line points to a subject segmentation map 
 input_txt = "/local/scratch/v_karthik_mohan/template.txt"
 
 labels = [0, 1, 2, 3]  
 # Labels in thisc case are Background, Cortex, Subcortical GM, White Matter, CSF
 
-# Function to convert to one-hot encoding
 def to_one_hot(segmentation, labels):
-    one_hot = np.zeros((len(labels), *segmentation.shape), dtype=np.uint8)  # Shape: (4, H, W, D)
+    one_hot = np.zeros((len(labels), *segmentation.shape), dtype=np.uint8)  
     for i, label in enumerate(labels):
         one_hot[i] = (segmentation == label).astype(np.uint8)
     return one_hot
 
-# Process each segmentation map
 with open(input_txt, 'r') as file:
     paths = file.read().splitlines()
 
 for path in tqdm(paths, desc="Processing segmentation maps"):
-    # Load segmentation map
     img = nib.load(path)
     seg_map = img.get_fdata().astype(np.int16) 
     one_hot_map = to_one_hot(seg_map, labels)

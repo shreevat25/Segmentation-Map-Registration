669eac09a68d6b016b9044f3b81bbf5845c0fbca
karthiknm <83635421+karthiknm@users.noreply.github.com>
2025-04-30 20:25:35 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,89 +1,109 @@
-import torch
+ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 class UNet(nn.Module):
     def __init__(self, in_channels, out_channels):
-        super(UNet, self).__init__()
-
-        def conv_block(in_channels, out_channels):
-            return nn.Sequential(
-                nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1),
-                nn.InstanceNorm3d(out_channels),
-                nn.ReLU(inplace=True),
-                nn.Conv3d(out_channels, out_channels, kernel_size=3, padding=1),
-                nn.InstanceNorm3d(out_channels),
-                nn.ReLU(inplace=True)
-            )
-
-        def upsample_block(in_channels, out_channels):
-            return nn.ConvTranspose3d(in_channels, out_channels, kernel_size=2, stride=2)
-
-        # Encoder
-        self.enc1 = conv_block(in_channels, 32)  # 8 channels: template + fixed given together as input 4 channels each
-        self.pool1 = nn.MaxPool3d(kernel_size=2, stride=2)
-        self.enc2 = conv_block(32, 64)
-        self.pool2 = nn.MaxPool3d(kernel_size=2, stride=2)
-        self.enc3 = conv_block(64, 128)
-        self.pool3 = nn.MaxPool3d(kernel_size=2, stride=2)
-        self.enc4 = conv_block(128, 256)
-        self.pool4 = nn.MaxPool3d(kernel_size=2, stride=2)
-
+        super().__init__()
         
-        self.bottleneck = conv_block(256, 512)
-
-        # Decoder
-        self.up4 = upsample_block(512, 256)
-        self.dec4 = conv_block(512, 256)
-        self.up3 = upsample_block(256, 128)
-        self.dec3 = conv_block(256, 128)
-        self.up2 = upsample_block(128, 64)
-        self.dec2 = conv_block(128, 64)
-        self.up1 = upsample_block(64, 32)
-        self.dec1 = conv_block(64, 32)
-
+        self.encoder = nn.ModuleDict({
+            'enc1': self._block(in_channels, 32),
+            'enc2': self._block(32, 64),
+            'enc3': self._block(64, 128),
+            'enc4': self._block(128, 256)
+        })
         
-        self.out_conv = nn.Conv3d(32, 3, kernel_size=1)  # 3 channels for deformation field (x, y, z)
+        self.bottleneck = self._block(256, 512)
+        self.pool = nn.MaxPool3d(2, 2)
+        self.decoder = nn.ModuleDict({
+            'dec4': self._block(512, 256),
+            'dec3': self._block(256, 128),
+            'dec2': self._block(128, 64),
+            'dec1': self._block(64, 32)
+        })
+        
+        self.upsample = nn.ModuleDict({
+            'up4': self._upsample(512, 256),
+            'up3': self._upsample(256, 128),
+            'up2': self._upsample(128, 64),
+            'up1': self._upsample(64, 32)
+        })
+        
+        self.out_conv = nn.Sequential(
+            nn.Conv3d(32, 32, 3, padding=1),
+            nn.GroupNorm(8, 32),
+            nn.LeakyReLU(0.2),
+            nn.Conv3d(32, 3, 1, bias = False),
+            nn.Tanh()
+        )
+
+    def _block(self, in_ch, out_ch):
+        return nn.Sequential(
+            nn.Conv3d(in_ch, out_ch, 3, padding=1),
+            nn.GroupNorm(8, out_ch),
+            nn.LeakyReLU(0.2),
+            nn.Conv3d(out_ch, out_ch, 3, padding=1),
+            nn.GroupNorm(8, out_ch),
+            nn.LeakyReLU(0.2)
+        )
+    
+    def _upsample(self, in_ch, out_ch):
+        return nn.ConvTranspose3d(in_ch, out_ch, 2, stride=2)
+
 
     def forward(self, x):
-        # Encoder part
-        e1 = self.enc1(x)
-        p1 = self.pool1(e1)
-        e2 = self.enc2(p1)
-        p2 = self.pool2(e2)
-        e3 = self.enc3(p2)
-        p3 = self.pool3(e3)
-        e4 = self.enc4(p3)
-        p4 = self.pool4(e4)
+        # Encoder
+        e1 = self.encoder['enc1'](x)
+        p1 = self.pool(e1)
+        
+        e2 = self.encoder['enc2'](p1)
+        p2 = self.pool(e2)
+        
+        e3 = self.encoder['enc3'](p2)
+        p3 = self.pool(e3)
+        
+        e4 = self.encoder['enc4'](p3)
+        p4 = self.pool(e4)
 
+        # Bottleneck
         b = self.bottleneck(p4)
 
-        #Decoder part
-        up4 = self.up4(b)
-        d4 = self.dec4(torch.cat((up4, e4), dim=1))
-        up3 = self.up3(d4)
-        d3 = self.dec3(torch.cat((up3, e3), dim=1))
-        up2 = self.up2(d3)
-        d2 = self.dec2(torch.cat((up2, e2), dim=1))
-        up1 = self.up1(d2)
-        d1 = self.dec1(torch.cat((up1, e1), dim=1))
+        # Decoder
+        up4 = self.upsample['up4'](b)
+        d4 = self.decoder['dec4'](torch.cat([up4, e4], dim=1))
+        
+        up3 = self.upsample['up3'](d4)
+        d3 = self.decoder['dec3'](torch.cat([up3, e3], dim=1))
+        
+        up2 = self.upsample['up2'](d3)
+        d2 = self.decoder['dec2'](torch.cat([up2, e2], dim=1))
+        
+        up1 = self.upsample['up1'](d2)
+        d1 = self.decoder['dec1'](torch.cat([up1, e1], dim=1))
 
-        # deformation_field = self.out_conv(d1)
-        deformation_field = self.out_conv(d1)
-        return deformation_field
-
+        # Final output
+        return self.out_conv(d1)
+    
 class SpatialTransformer(nn.Module):
     def __init__(self):
-        super(SpatialTransformer, self).__init__()
-
+        super().__init__()
+        
     def forward(self, moving, deformation):
- 
-        B, C, H, W, D = moving.shape
-        grid = F.affine_grid(torch.eye(3, 4).unsqueeze(0).repeat(B, 1, 1).to(moving.device),
-                             moving.size(), align_corners=False)
+        # Generate grid dynamically for device compatibility
+        B, _, H, W, D = moving.shape
+        grid = F.affine_grid(
+            torch.eye(3,4, device=moving.device).unsqueeze(0).repeat(B,1,1),
+            moving.shape,
+            align_corners=False
+        )
 
-        deformation = deformation.permute(0, 2, 3, 4, 1)  # (B, H, W, D, 3)
-        warped_grid = grid + deformation
-
-        warped = F.grid_sample(moving, warped_grid, mode='bilinear', padding_mode='border', align_corners=False)
-        return warped
+       
+        warped_grid = grid + deformation.permute(0,2,3,4,1) 
+        warped = F.grid_sample(
+            moving,
+            warped_grid,
+            mode='bilinear',  # 
+            padding_mode='border',
+            align_corners=False
+        )
+        return warped 
